332905192da5a31ebf3726bca4c39b9f844ed783
Anastasia korinkova <Anastasia@rrsystems.co.il>
2024-08-14 16:12:50 +0300

    express fix 5

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Configuration options go here
-  server: {
-    port: 3000, // Установить порт на 3000
-  },
   build: {
     assetsDir: 'assets',
   },
