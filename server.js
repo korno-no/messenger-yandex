8c45a4add4146359f1feb239e8a2d3d902f781e0
Anastasia korinkova <Anastasia@rrsystems.co.il>
2024-08-14 16:16:19 +0300

    express fix 6

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const path = require('path');
 
-// Создание экземпляра приложения
 const app = express();
 
-// Определение порта
 const PORT = process.env.PORT || 3000;
 
-// Определение текущего модуля
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(express.static('dist'));
 
-// Использование папки dist как статической
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Обработка всех запросов и отдача index.html
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Example app listening on port http://localhost:${PORT}/`);
 });
