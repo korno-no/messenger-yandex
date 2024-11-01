031e8e4d7340ec11ae057974606dfc30556962d9
Korno <korno9612@gmail.com>
2024-11-01 13:51:05 +0200

    fix types in event-bus

--- src/core/event-bus.ts
+++ src/core/event-bus.ts
@@ -1,27 +1,27 @@
 export default class EventBus<E extends string> {
-  listeners: {[key in E]?: Function[]} = {};
+  listeners: { [key in E]?: Array<(...args: unknown[]) => void> } = {};
 
-  on<F extends(...args: Parameters<F>) => void>(event: E, callback: F) {
+  on(event: E, callback: (...args: unknown[]) => void) {
     if (!this.listeners[event]) {
       this.listeners[event] = [];
     }
         this.listeners[event]!.push(callback);
   }
 
-  off<F extends(...args: any) => void>(event: E, callback: F) {
+  off(event: E, callback: (...args: unknown[]) => void) {
     if (!this.listeners[event]) {
       throw new Error(`Нет события: ${event}`);
     }
     this.listeners[event] = this.listeners[event]!.filter((listener) => listener !== callback);
   }
 
-  emit<F extends(...args: any) => void>(event: E, ...args: Parameters<F>) {
+  emit(event: E, ...args: unknown []) {
     if (!this.listeners[event]) {
       return;
       // throw new Error(`Нет события: ${event}`);
     }
     this.listeners[event]!.forEach((listener) => {
-      listener(...args as Parameters<F>) ;
+      listener(...args) ;
     });
   }
 }
