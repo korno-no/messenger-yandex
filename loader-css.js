a8ce4fc1e2eaa21cf25102b18315b9a31800d528
Korno <korno9612@gmail.com>
2024-12-06 03:37:30 +0200

    fix loadre.css

--- loader-css.js
+++ loader-css.js
@@ -1,24 +1,20 @@
-import fs from 'node:fs/promises';
-import { fileURLToPath } from 'node:url';
-
 export async function resolve(specifier, context, next) {
-	const nextResult = await next(specifier, context);
+  const nextResult = await next(specifier, context);
 
-	if (!specifier.endsWith('.css')) return nextResult;
+  if (!specifier.endsWith('.css')) return nextResult;
 
-	return {
-		format: 'css',
-		shortCircuit: true,
-		url: nextResult.url,
-	};
+  return {
+  	format: 'css',
+  	shortCircuit: true,
+  	url: nextResult.url,
+  };
 }
 
 export async function load(url, context, next) {
-	if (context.format !== 'css') return next(url, context);
-
-	return {
-		format: 'module',
-		shortCircuit: true,
-		source: '',
-	};
+  if (context.format !== 'css') return next(url, context);
+  return {
+  	format: 'module',
+  	shortCircuit: true,
+  	source: '',
+  };
 }
