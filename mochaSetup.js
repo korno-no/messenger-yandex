ff09fb20298119b50942d3c5a18e85061d4fa132
Korno <korno9612@gmail.com>
2024-12-06 02:28:02 +0200

    add mocha test

---
+++ mochaSetup.js
@@ -0,0 +1,9 @@
+import jsdom from 'jsdom';
+
+const { JSDOM } = jsdom;
+const dom = new JSDOM('<body></body>', { url: "http://localhost" });
+
+global.window = dom.window;
+global.document = dom.window.document;
+global.Node = window.Node;
+global.history = dom.window.history;
